cf21cc6fb4b1b680385f402ee317cf6bdf7c9fba
DevZarah <mojeedomowunmi3@gmail.com>
2023-05-17 18:34:25 +0100

    icons

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+// const searchInput = document.getElementById('dataSearch')
+// const filter = input.value.toUpperCase();
+// searchInput.addEventListener("input" ,(e)=>{
+// const value = e.target.value
+// console.log(value)
+// })
+
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open')
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+
+
